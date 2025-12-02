c4ec196949e195dd4e566be6ecd6eb35b80a607c
dyxin <2232591215@qq.com>
2025-12-02 22:10:16 +0800

    feat:新增消息查找功能

--- community/views.py
+++ community/views.py
@@ -14,3 +14,12 @@ class CommunityViewSet(viewsets.ViewSet):
         # url = 'http://127.0.0.1:8000/media/audio/ceshi.mp3'
         res_json = {'data': url, 'msg': 'ok', 'code': 1}
         return Response(res_json)
+
+    def putAudio(self, request):
+        mtext = '未查到'
+        if 'text' in request.data:
+            mtext = request.data['text']
+        url = audioex.getAudio(mtext)
+        # url = 'http://127.0.0.1:8000/media/audio/ceshi.mp3'
+        res_json = {'data': url, 'msg': 'ok', 'code': 1}
+        return Response(res_json)
