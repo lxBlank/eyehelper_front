a2e2cea28ed165d77af191628975ee60d1e5600c
shuwu000 <1448278818@qq.com>
2025-12-02 22:40:58 +0800

    加上注释9

--- ceshi/models.py
+++ ceshi/models.py
@@ -5,4 +5,6 @@ class Product(models.Model):
     name = models.CharField(max_length=100)
     price = models.FloatField()
 
+class Order(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
 
